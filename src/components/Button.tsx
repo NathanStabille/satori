b8e60b81e5e4f908795b08a3797bba989467d74f
NathanStabille <nathan.stabille@hotmail.com>
2024-11-18 14:08:29 -0300

    font add

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,7 +16,7 @@ export const Button = ({
 }: IButtonProps) => {
   return (
     <button
-      className={`flex select-none items-center justify-center gap-2 rounded-xl bg-lightPrimarColor/20 px-3 py-2 font-baiJamjuree text-[16px] font-medium text-lightPrimarColor outline-none transition-all hover:bg-lightPrimarColor hover:text-slate-50 active:bg-indigo-600 dark:border-darkPrimaryColor dark:bg-darkPrimaryColor/10 dark:text-darkPrimaryColor dark:hover:bg-darkPrimaryColor dark:hover:text-[#1e1e1e] dark:active:bg-red-600 ${className}`}
+      className={`flex select-none items-center justify-center gap-2 rounded-xl bg-lightPrimarColor/20 px-3 py-2 font-baiJamjuree text-[16px] text-lightPrimarColor outline-none transition-all hover:bg-lightPrimarColor hover:text-slate-50 active:bg-indigo-600 dark:border-darkPrimaryColor dark:bg-darkPrimaryColor/10 dark:text-darkPrimaryColor dark:hover:bg-darkPrimaryColor dark:hover:text-[#1e1e1e] dark:active:bg-red-600 ${className}`}
       {...rest}
     >
       {iconBefore}
