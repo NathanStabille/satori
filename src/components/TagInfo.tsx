4d8b1651cd272e068fd5f083e3fbaada1ee48fe8
NathanStabille <nathan.stabille@hotmail.com>
2024-11-18 14:13:01 -0300

    font add

--- src/components/TagInfo.tsx
+++ src/components/TagInfo.tsx
@@ -10,7 +10,7 @@ export const TagInfo = ({ name, className, icon, ...rest }: ITagInfoProps) => {
   return (
     <label
       {...rest}
-      className={`border-lightPrimarColor text-lightPrimarColor dark:border-darkPrimaryColor dark:text-darkPrimaryColor select-none rounded-lg border p-[5px] px-2 py-1 font-baiJamjuree text-[16px] font-medium ${className}`}
+      className={`border-lightPrimarColor text-lightPrimarColor dark:border-darkPrimaryColor dark:text-darkPrimaryColor select-none rounded-lg border p-[5px] px-2 py-1 font-baiJamjuree text-[16px] ${className}`}
     >
       {name}
       {icon}
